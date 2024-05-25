e380454128026419831e767191b81aab593685b3
sashel007 <sashel007@yandex.ru>
2024-05-25 09:02:29 +0300

    label changed_2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "ru.sashel007.quitsmoking"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
